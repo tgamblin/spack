43421d51adb5c60fea3a96389d3cc6eea1f01413
Todd Gamblin <tgamblin@llnl.gov>
2021-07-19 14:15:35 -0700

    add some unwanted space between imports

--- lib/spack/spack/spec.py
+++ lib/spack/spack/spec.py
@@ -95,6 +95,9 @@ import spack.architecture
 import spack.compiler
 import spack.compilers
 import spack.config
+
+
+
 import spack.dependency as dp
 import spack.error
 import spack.hash_types as ht
